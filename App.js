f6415d299fad84c504f36df48cb4a3774324bf11
Nguyen Kien <ngkien299@gmail.com>
2023-06-15 13:43:32 +0700

    First intial for orchid shop

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  HomeScreen,
+  LoginScreen,
+  OrchidDetailScreen,
+  OrchidListScreen,
+} from './src/screens';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Login' component={HomeScreen} />
+        <Stack.Screen name='OrchidList' component={OrchidListScreen} />
+        <Stack.Screen name='OrchidDetail' component={OrchidDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import LoginScreen from './LoginScreen';
+import HomeScreen from './HomeScreen';
+import OrchidDetailScreen from './OrchidDetailScreen';
+import OrchidListScreen from './OrchidListScreen';
+
+export { LoginScreen, HomeScreen, OrchidDetailScreen, OrchidListScreen };
