e1677744a7a7d798f70e815508d655a5250d9d82
Nguyen Kien <ngkien299@gmail.com>
2023-06-15 13:36:25 +0700

    Initial commit
    Generated by create-expo-app 2.0.3.

---
+++ src/screens/OrchidDetailScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const OrchidDetailScreen = () => {
+  return (
+    <View>
+      <Text>OrchidDetailScreen</Text>
+    </View>
+  );
+};
+
+export default OrchidDetailScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/OrchidListScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const OrchidListScreen = () => {
+  return (
+    <View>
+      <Text>OrchidListScreen</Text>
+    </View>
+  );
+};
+
+export default OrchidListScreen;
+
+const styles = StyleSheet.create({});
